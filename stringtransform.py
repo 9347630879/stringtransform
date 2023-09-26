c4f56b1bf0fec42b7da32a6ec9a7c263b48ead08
9347630879 <145848207+9347630879@users.noreply.github.com>
2023-09-26 16:10:25 +0530

    Add files via upload

---
+++ stringtransform.py
@@ -0,0 +1,108 @@
+#string transform method
+# capitalize
+a = "who are you"
+print(a.capitalize())
+
+#title
+
+a = "dhanunjai sai sundhar"
+print(a.title())
+
+#upper
+
+a = "dhanunjai sai sundhar"
+print(a.upper())
+
+#lower
+
+a = "DHANUNJAI SAI SUNDHAR"
+print(a.lower())
+
+#swap case
+a = "sundhar"
+print(a.swapcase())
+
+#casefold
+a = "DHANUNJAI SAI SUNDHAR"
+print(a.casefold())
+
+
+
+#string check function
+#numeric
+a = "9347630879"
+print(a.isnumeric())
+
+a = "sai"
+print(a.isnumeric())
+
+#alphanumeric
+a = "sai2563"
+print(a.isalnum())
+
+a = "s ai"
+print(a.isalnum())
+
+#isdigiit
+a = "985685858"
+print(a.isdigit())
+
+a = "dhanunjai"
+print(a.isdigit())
+
+#isdeecimal
+a = "12058"
+print(a.isdecimal())
+
+#isasscii
+a = "98756"
+print(a.isascii())
+
+a = "sai"
+print(a.isascii())
+
+#isupper
+a = "SAI"
+print(a.isupper())
+
+a = "sai"
+print(a.isupper())
+
+#islower
+a = "SAI"
+print(a.islower())
+
+
+a = "sai"
+print(a.islower())
+
+#isspace
+a = " "
+print(a.isspace())
+
+
+a = "sai"
+print(a.isspace())
+
+#isidentifes
+a = "@#$%"
+print(a.isidentifier())
+
+a = "sai"
+print(a.isidentifier())
+
+#isprintable
+a = "sundhar"
+print(a.isprintable())
+
+a = "934763879"
+print(a.isprintable())
+
+
+#istitle
+a = "dhanunnjai sai sundhar"
+print(a.istitle())
+
+
+a = "Dhanunjai Sai Sundhar"
+print(a.istitle())
